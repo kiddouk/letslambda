c8013809423fb89e9a07ebc5cd3e36d11e61f8b2
Aurélie Requiem <aurelien@requiem.fr>
2016-06-04 23:36:54 +0000

    added AES encryption class (partial)

--- letslambda.py
+++ letslambda.py
@@ -2,6 +2,8 @@
 
 import yaml
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto import Random
 from boto import s3
 from boto import iam
 from boto import route53
@@ -31,6 +33,29 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 # add ch to logger
 LOG.addHandler(handler)
+
+
+class AESCipher:
+    def __init__( self, key ):
+        self.key = key
+
+    def encrypt( self, raw ):
+        raw = self._pad(raw)
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return base64.b64encode( iv + cipher.encrypt( raw ) )
+
+    def decrypt( self, enc ):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return self._unpad(cipher.decrypt( enc[AES.block_size:] ))
+
+    def _pad(self, s):
+        return s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
+
+    def _unpad(self, s):
+        return s[:-ord(s[len(s)-1:])]
 
 def load_config(bucket):
     """
