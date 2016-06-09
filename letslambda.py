4e046c454182f81bd97fa2b36cbaea409a9bd77d
Aurélie Requiem <aurelien@requiem.fr>
2016-06-09 00:47:32 +0000

    removed 'AESCipher' class since encryption will be done via S3-SSE+KMS for now

--- letslambda.py
+++ letslambda.py
@@ -39,28 +39,6 @@ handler.setFormatter(formatter)
 LOG.addHandler(handler)
 
 
-class AESCipher:
-    def __init__( self, key ):
-        self.key = key
-
-    def encrypt( self, raw ):
-        raw = self._pad(raw)
-        iv = Random.new().read( AES.block_size )
-        cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw ) )
-
-    def decrypt( self, enc ):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return self._unpad(cipher.decrypt( enc[AES.block_size:] ))
-
-    def _pad(self, s):
-        return s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
-
-    def _unpad(self, s):
-        return s[:-ord(s[len(s)-1:])]
-
 def load_config(bucket):
     """
     Try to load the letlambda.yml out of the user bucket
